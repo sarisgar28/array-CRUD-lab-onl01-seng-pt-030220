3c3f66e1929421e9f75fdf8d59eb897c5f591adb
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-09 04:04:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def add_element_to_start_of_array(array, element)
 def remove_element_from_end_of_array(array,name)
   array = ["array","name"]
   array.pop
-  puts array.inspect
+  return array 
   end
 
 def remove_element_from_start_of_array(array)
