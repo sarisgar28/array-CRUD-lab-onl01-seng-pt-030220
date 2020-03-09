73521365a0b5eefe70b3ae689f49c809fe0e621f
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-09 03:57:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,7 @@ def add_element_to_end_of_array(array,element)
 
 def add_element_to_start_of_array(array, element)
   array = ["array","element"]
-  return.unsift(element)
+  return.unsift "element"
   end 
 
 def remove_element_from_end_of_array(array)
