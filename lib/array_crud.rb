dee50df995c698b104cdd704efd4ae253de90c0d
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-09 05:16:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,7 +37,7 @@ def retrieve_element_from_index(array, index_number)
 def retrieve_first_element_from_array(array)
  array = ["array"]
  array = [0]
-return "array"
+return "0"
 end
 
 def retrieve_last_element_from_array(array)
