91d4a282f4d589dcfeb93f367073e41d26a11db0
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-09 00:18:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   my_array = ["1","2","3","4"]
   end
 
-def add_element_to_end_of_array(array,element,element)
+def add_element_to_end_of_array(array,element, element)
     array = ["array","element"]
     array << "element"
     puts array.inspect 
