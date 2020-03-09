5ad6084ff94c71cf9c9ba5bef3ec418eb61b7453
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-09 04:40:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,14 +20,15 @@ def add_element_to_start_of_array(array, element)
   end 
 
 def remove_element_from_end_of_array(array)
-  array = ["arrays!"]
+    array = ["arrays!"]
     array.pop
     end
 
 def remove_element_from_start_of_array(array)
-  
-  end
-
+    array = ["array"]
+    array.shift 
+    end
+end 
 def retrieve_element_from_index(array, index_number)
   
     end
