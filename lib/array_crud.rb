a5c70eb0fe45a6cc856c3f5cd25519dd7c252888
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-09 04:50:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,8 @@ def remove_element_from_start_of_array(array)
     array.shift
     end
 def retrieve_element_from_index(array, index_number)
-  
+    my_array = ["cat""cat_2"]
+    
     end
  
 def retrieve_first_element_from_array(array)
